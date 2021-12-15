d988e8350f24de1360b3440d89cd1ff6dc5d419e
Markus Kerle <m.kerle@jeanmueller.de>
2021-12-15 12:29:53 +0100

    add ini file reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include "Formula.h"
+#include "SimpleIni.h"
 
 int main() {
     std::cout << "Bla: " << Formula::bla(2) << std::endl;
+    
+    CSimpleIniA ini;
+	ini.SetUnicode();
+    
+    SI_Error rc = ini.LoadFile("example.ini");
+	if (rc < 0) { /* handle error */ };
+
+	const char* pv;
+	pv = ini.GetValue("section", "key1", "default");
+    std::cout << "ini1: " << pv << std::endl;
+
+	pv = ini.GetValue("section", "key2", "default");
+    std::cout << "ini2: " << pv << std::endl;
+
     return 0;
 }
